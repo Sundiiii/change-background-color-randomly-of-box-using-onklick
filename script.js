4cd94961a676683dc1084839bb9d055068b893af
Sundar  Deshmukh <109438264+Sundiiii@users.noreply.github.com>
2023-04-12 08:13:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ v+=Math.random().toString(16).slice(2,8);
 // document.write(v);
   box.style.backgroundColor=v;
 };
+document.getElementById("btn").onclick=changecolor;
+const box=document.querySelector('#outer');
+
+function changecolor(){
+    let x='#';
+    x+=Math.random().toString(16).slice(2,8);
+    box.style.backgroundColor=x;
